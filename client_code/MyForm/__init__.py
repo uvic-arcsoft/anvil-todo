39419ad6c45cecfe1f9a5eca75444c46460dac00
toanhminh0412@gmail.com <toanhminh0412@gmail.com>
2025-08-27 18:10:18 +0000

    Created MyForm

---
+++ client_code/MyForm/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import MyFormTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from .. import Form1
+
+
+class MyForm(MyFormTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    open_form('Form1')
